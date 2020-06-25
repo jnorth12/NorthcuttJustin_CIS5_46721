c6debaa6cd81a5de7b68947b0c154a155b41943b
jnorth12 <justinnorthcutt12@gmail.com>
2020-06-25 11:47:29 -0700

    Signed-off-by: jnorth12 <justinnorthcutt12@gmail.com>

---
+++ IntroLab/Assignment_1_JRN.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   Assignment_1_JRN.cpp
+ * Author: Justin Northcutt
+ *
+ * Created on June 22, 2020, 1:34 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
